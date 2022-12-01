e37aaa8c8c54cbef86a5e35fbfc076df920dcdb3
ValtierraXavier <xavier.valtierra@icloud.com>
2022-12-01 15:41:17 -0500

    Add css and add some java logic

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-for (i=1; i<=5;i++){
- 
-}
+let bananaFruit = document.getElementById('banana');
+let orangeFruit = document.getElementById('orange');
+let appleFruit = document.getElementById('apple');
+let grapesFruit = document.getElementById('grapes');
+let strawberryFruit = document.getElementById('strawberry');
+
+const symbols = [bananaFruit,orangeFruit,appleFruit,grapesFruit,strawberryFruit];
+
