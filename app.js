a5b9067040ae3bdf30afe5f23ea8ea8faff65761
ValtierraXavier <xavier.valtierra@icloud.com>
2022-12-02 15:22:14 -0500

    managed to get the drum symbols to change asyncronously; need to make drum2 repeat through loop

--- app.js
+++ app.js
@@ -6,6 +6,28 @@ const symbolsObj ={
     eSymbol:"E"
 };
 
+function drum1Speed(ms){
+    return new Promise(resolve =>{
+        
+        setTimeout(()=>{resolve('')}, ms);
+})
+}
+
+function drum2Speed(ms){
+    return new Promise(resolve =>{
+        
+        setTimeout(()=>{resolve('')}, ms);
+})
+}
+
+function drum3Speed(ms){
+    return new Promise(resolve =>{
+        
+        setTimeout(()=>{resolve('')}, ms);
+})
+}
+
+async function spinDrum1(){
 for(let i=0;i<=4;i++){
     if (document.getElementById('firstDrumSymbol').innerText === symbolsObj.aSymbol){
         document.getElementById('firstDrumSymbol').innerText = symbolsObj.bSymbol;
@@ -19,34 +41,48 @@ for(let i=0;i<=4;i++){
     }else if(document.getElementById('firstDrumSymbol').innerText === symbolsObj.eSymbol){
         document.getElementById('firstDrumSymbol').innerText = symbolsObj.aSymbol;
     }
+    await drum2Speed(1000);
     //console.log(document.getElementById('firstDrumSymbol').innerText);
+    i = i-4;
 }
+}
+spinDrum1();
 
-// setInterval(changeLetter, 3000);
-// drumSymArr = {
-//     drum1: document.getElementById('firstDrumSymbol'),
-//     drum2: document.getElementById('secondDrumSymbol'),
-//     drum3: document.getElementById('thirdDrumSymbol')
+async function spinDrum2(){
+for(let j=0;j<=4;j++){
+    if (document.getElementById('firstDrumSymbol').innerText === symbolsObj.aSymbol){
+        document.getElementById('firstDrumSymbol').innerText = symbolsObj.bSymbol;
+
+    }   else if(document.getElementById('secondDrumSymbol').innerText === symbolsObj.bSymbol){
+        document.getElementById('secondDrumSymbol').innerText = symbolsObj.cSymbol;
+    }else if(document.getElementById('secondDrumSymbol').innerText === symbolsObj.cSymbol){
+        document.getElementById('secondDrumSymbol').innerText = symbolsObj.dSymbol;
+    } else if(document.getElementById('secondDrumSymbol').innerText === symbolsObj.dSymbol){
+        document.getElementById('secondDrumSymbol').innerText = symbolsObj.eSymbol;
+    }else if(document.getElementById('secondDrumSymbol').innerText === symbolsObj.eSymbol){
+        document.getElementById('secondDrumSymbol').innerText = symbolsObj.aSymbol;
+    }
+    await drum2Speed(2000);
+    console.log(document.getElementById('secondDrumSymbol').innerText);
+    j=j-4;
+}
+}
+spinDrum2();
+
+
+// for(let i=0;i<=4;i++){
+//     if (document.getElementById('thirdDrumSymbol').innerText === symbolsObj.aSymbol){
+//         document.getElementById('thirdDrumSymbol').innerText = symbolsObj.bSymbol;
+
+//     }   else if(document.getElementById('firstDrumSymbol').innerText === symbolsObj.bSymbol){
+//         document.getElementById('thirdDrumSymbol').innerText = symbolsObj.cSymbol;
+//     }else if(document.getElementById('thirdDrumSymbol').innerText === symbolsObj.cSymbol){
+//         document.getElementById('thirdDrumSymbol').innerText = symbolsObj.dSymbol;
+//     } else if(document.getElementById('thirdDrumSymbol').innerText === symbolsObj.dSymbol){
+//         document.getElementById('thirdDrumSymbol').innerText = symbolsObj.eSymbol;
+//     }else if(document.getElementById('thirdDrumSymbol').innerText === symbolsObj.eSymbol){
+//         document.getElementById('thirdDrumSymbol').innerText = symbolsObj.aSymbol;
+//     }
+//     //console.log(document.getElementById('thirdDrumSymbol').innerText);
 // }
-// //console.log(symbolsArr);
-//  let first = document.getElementById('firstDrumSymbol').innerHTML.style.color='red';
-// console.log(drumSym1);
 
-// drumSym1.style.color='red';
-// const drumSym2 = document.getElementById('secondDrumSymbol');
-// const drumSym3 = document.getElementById('thirdDrumSymbol');
-
-
-
-
-
-// if (drumSym1===drumSym2 && drumSym1==drumSym3){
-//     console.log("Jackpot!");
-// }
-// if (drumSym2===drumSym1 && drumSym2===drumSym3){
-// console.log('Jackpot!');
-// }
-
-// if (drumSym3===drumSym2 && drumSym3===drumSym1){
-// console.log('Jackpot!');
-// }
